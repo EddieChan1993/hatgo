19e5f24a9b6b8f766e6dca0f7b2120f1d25dadd8
EddieThunerobot <dcwen666666@163.com>
2018-08-12 15:07:20 +0800

    优化

---
+++ endless_test.go
@@ -0,0 +1,52 @@
+package main_test
+
+import (
+	"testing"
+	"log"
+	"syscall"
+	"sdog/models"
+	"sdog/logging"
+	"sdog/pkg/setting"
+	"sdog/routers"
+)
+
+var _version_ ="none setting"
+
+func testEndLess(T *testing.T) {
+	defer func() {
+		models.Engine.Close()
+		logging.Logs.Close()
+		logging.SqlLogs.Close()
+	}()
+
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+
+	server := endless.NewServer(setting.HTTPPort, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Println("server is running in", setting.RunMode)
+		log.Println("Listening port", setting.HTTPPort)
+		log.Println("Actual pid is", syscall.Getpid())
+	}
+	setting.VersionShow(_version_)
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("Server err:%v", err)
+	}
+}
+
+func testNoEndless(T testing.T)  {
+	defer func() {
+		models.Engine.Close()
+		logging.Logs.Close()
+		logging.SqlLogs.Close()
+	}()
+	router := routers.InitRouter()
+	setting.VersionShow(_version_)
+	err := router.Run(setting.HTTPPort)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
