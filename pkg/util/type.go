7500a9f4820502e5b95f6432c7e95547b1f9997c
tyria-pc <tyria-pc@example.com>
2018-09-17 15:45:38 +0800

    优化

--- pkg/util/type.go
+++ pkg/util/type.go
@@ -4,44 +4,43 @@
 package util
 
 import (
-	"log"
 	"strconv"
 )
 
 //string->float64
-func StringToFloat64(str string) float64 {
-	floatVal,err:=strconv.ParseFloat(str, 64)
+func StringToFloat64(str string) (float64, error) {
+	floatVal, err := strconv.ParseFloat(str, 64)
 	if err != nil {
-		log.Fatal(err)
+		return 0, err
 	}
-	return floatVal
+	return floatVal, nil
 }
 
 //string->int64
-func StringToInt64(str string) int64 {
+func StringToInt64(str string) (int64, error) {
 	intVal, err := strconv.ParseInt(str, 10, 64)
 	if err != nil {
-		log.Fatal(err)
+		return 0, err
 	}
-	return intVal
+	return intVal, nil
 }
 
 //float64->int64
-func Float64ToInt64(floatParam float64) int64 {
-	str:=strconv.FormatFloat(floatParam, 'f', -1, 64)
+func Float64ToInt64(floatParam float64) (int64, error) {
+	str := strconv.FormatFloat(floatParam, 'f', -1, 64)
 	return StringToInt64(str)
 }
 
 //int->float64
-func IntToFloat64(intParam int)float64 {
-	i:=int64(intParam)
-	str:=strconv.FormatInt(i, 10)
+func IntToFloat64(intParam int) (float64, error) {
+	i := int64(intParam)
+	str := strconv.FormatInt(i, 10)
 	return StringToFloat64(str)
 }
 
 //int64->float64
-func Int64ToFloat64(intParam int64) float64 {
-	str:=strconv.FormatInt(intParam, 10)
+func Int64ToFloat64(intParam int64) (float64, error) {
+	str := strconv.FormatInt(intParam, 10)
 	return StringToFloat64(str)
 }
 
