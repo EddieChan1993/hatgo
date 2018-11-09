1c5339f30d4ada71793750c300d7cdb8c817bcd3
tyria-pc <tyria-pc@example.com>
2018-11-09 16:02:40 +0800

    优化

---
+++ ext/weixin/conf.go
@@ -0,0 +1,9 @@
+package weixin
+
+const (
+	appid       = "wxabbfe2d69e9264de"
+	appSecret   = "4962407af122edd415d779762034b375"
+	mchId       = "1517821541"
+	wxPayApiKey = "qianbaike666qianbaike666qianbaik"
+	tradeType   = "JSAPI"
+)
